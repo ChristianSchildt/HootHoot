20eaa20404e869b3b01332e468bd92b44cccf33d
Chris-1993 <23014876+Chris-1993@users.noreply.github.com>
2023-03-15 17:44:06 +0100

    Update GameSession.js

--- main/server/game-session/GameSession.js
+++ main/server/game-session/GameSession.js
@@ -211,7 +211,7 @@ class GameSession {
         try{
             //Array zum JSON string machen
             const playerTimesJson = JSON.stringify(gameResults);
-            await pool.query("INSERT INTO game_session (question_id, player_times) VALUES ($1,$2)",[questionId, playerTimesJson]);
+            //await pool.query("INSERT INTO game_session (question_id, player_times) VALUES ($1,$2)",[questionId, playerTimesJson]);
         }catch(e){
             console.log(e);
         }
